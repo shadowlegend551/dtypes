a19b174443ea7b975d039f435782d9e75619feba
shadowlegend551 <oskari.j.lappalainen@gmail.com>
2024-07-20 00:40:10 +0300

    added file and folder checking to script

--- scripts/wrapper_gen.py
+++ scripts/wrapper_gen.py
@@ -1,11 +1,27 @@
-import os
+# RUN THIS FILE FROM THE PROJECTS ROOT DIRECTORY.
 
+
+import os
+import sys
 
 
 def main():
-    if os.path.isdir('include'):
-        exit('No folder called include.')
+    if not os.path.isdir('include'):
+        return 'No folder called include.'
+    if not os.path.isdir('wrappers'):
+        return 'No folder called wrappers.'
+
+    args = sys.argv[1:]
+    if not len(args):
+        return 'No arguments provided'
+
+    template_file = 'scripts/templates/' + args.pop(0)
+    if not os.path.isfile(template_file):
+        return f'No template file "{template_file}"'
 
 
+
+   
 if __name__ == '__main__':
-    main()
+    exit_message = main()
+    print(exit_message)
