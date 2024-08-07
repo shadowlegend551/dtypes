2a71d5a88ead7b1ace45db54ccac11c5b888a844
shadowlegend551 <oskari.j.lappalainen@gmail.com>
2024-08-07 23:17:02 +0300

    added rest of function declarations

--- include/stackcore.h
+++ include/stackcore.h
@@ -13,8 +13,25 @@ typedef struct Stack
 
 } Stack;
 
+// Returns a pointer to a new stack.
 Stack* new_stack(size_t item_size);
 
+// Deletes a stack along with it's data.
 void deletestack(Stack* stack);
+
+// Pushes data on top of stack.
+int spush(Stack* stack, void* data);
+
+// Returns the top of stack and removes it from stack.
+void* spop(Stack* stack);
+
+// Returns the topmost data from stack without deleting it.
+void* sgettop(Stack* stack);
+
+// Gets the bottommost data.
+void* sgetbase(Stack* stack);
+
+// Returns the length (height) of a stack.
+int sgetlen(Stack* stack);
 
 #endif
